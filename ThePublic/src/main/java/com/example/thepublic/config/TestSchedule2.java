3b200ed216134ea1dbb05241d38a6898c96c4f99
laibinxie <113881675+laibinxie@users.noreply.github.com>
2022-09-19 17:30:44 +0800

    Add files via upload

---
+++ ThePublic/src/main/java/com/example/thepublic/config/TestSchedule2.java
@@ -0,0 +1,22 @@
+package com.example.thepublic.config;
+
+import com.example.thepublic.exception.MyHttpException;
+import com.example.thepublic.service.ThePublicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class TestSchedule2 {
+    @Autowired
+    private ThePublicService thePublicService;
+    //@Scheduled(cron = "0 30 23  * * ?")
+    @Scheduled(cron = "19 22 * * * *")
+    public void work() throws MyHttpException {
+        thePublicService.sendMessage();
+    }
+}
+
